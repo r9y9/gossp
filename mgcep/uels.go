009107dd22404bbb849b47544e5612c616a849db
Ryuichi Yamamoto <zryuichi@gmail.com>
2014-06-10 01:15:40 +0900

    Fix wrong function name of uels.go

--- mgcep/uels.go
+++ mgcep/uels.go
@@ -6,5 +6,5 @@ import (
 
 // TODO(ryuichi) replace with pure Go.
 func UELS(audioBuffer []float64, order int) []float64 {
-	return sptk.UELSSimple(audioBuffer, order)
+	return sptk.UELSWithDefaultParameters(audioBuffer, order)
 }
